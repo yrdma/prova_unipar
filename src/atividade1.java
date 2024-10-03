7b814f3c41f53af3f453d1873c6dd59e14a80ecb
gustavomullersouza <gustavomullerdesouza@gmail.com>
2024-10-02 21:11:52 -0300

    prova

--- src/atividade1.java
+++ src/atividade1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class atividade1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int alinhamentos = 0;
